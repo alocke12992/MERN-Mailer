5cb47bcea79f4892dec7eb9979f0905e011818cd
Andrew Locke <alocke12992@gmail.com>
2018-05-03 20:16:52 -0500

    Refactors server side

--- server/index.js
+++ server/index.js
@@ -1,38 +1,9 @@
 //using common JS modules b/c NodeJS does not provide support for ES6 imports
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20')
-  .Strategy;
-const keys = require('./config/keys');
+require('./services/passport');
+
 const app = express();
-
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback',
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log('access token:', accessToken);
-      console.log('refresh token:', refreshToken);
-      console.log('profile', profile);
-    },
-  ),
-);
-
-app.get(
-  '/auth/google',
-  passport.authenticate('google', {
-    // Specify what we are asking the user to allow us access
-    scope: ['profile', 'email'],
-  }),
-);
-
-app.get(
-  '/auth/google/callback',
-  passport.authenticate('google'),
-);
+require('./routes/authRoutes')(app);
 
 // Set up dynamic port to prep for Heroku deploy
 const PORT = process.env.PORT || 5000;
---
+++ server/routes/authRoutes.js
@@ -0,0 +1,16 @@
+const passport = require('passport');
+
+module.exports = app => {
+  app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+      // Specify what we are asking the user to allow us access
+      scope: ['profile', 'email'],
+    }),
+  );
+
+  app.get(
+    '/auth/google/callback',
+    passport.authenticate('google'),
+  );
+};
