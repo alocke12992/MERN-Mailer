27da03807fbc34213912ab22e1dd1dd5543c2578
Andrew Locke <alocke12992@gmail.com>
2018-05-07 17:00:39 -0600

    adds prod and dev keys

---
+++ server/config/prod.js
@@ -0,0 +1,10 @@
+// prod.js - production keys
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+};
+
+// clientID: '505463596052-ikuk3qnjaqlm8albd9lq7t703asou3ve.apps.googleusercontent.com'
+//clientSecret: S7J1xOS2hSK42FiwghailEBw
