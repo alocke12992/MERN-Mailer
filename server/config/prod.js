fa3814834a0b0da7cd3e496643c8e2bebcd0560e
Andrew Locke <alocke12992@gmail.com>
2018-05-08 11:18:44 -0600

    Adds stripe keys

--- server/config/prod.js
+++ server/config/prod.js
@@ -4,7 +4,6 @@ module.exports = {
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
+  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
 };
-
-// clientID: '505463596052-ikuk3qnjaqlm8albd9lq7t703asou3ve.apps.googleusercontent.com'
-//clientSecret: S7J1xOS2hSK42FiwghailEBw
