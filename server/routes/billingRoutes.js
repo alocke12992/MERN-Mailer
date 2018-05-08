0b0b4abc56fdc251b830d4e78d0999cb4a4b2602
Andrew Locke <alocke12992@gmail.com>
2018-05-08 16:43:53 -0600

    Adds auth middleware

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -1,16 +1,22 @@
 //by default express does not parse the request payload
 const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
+const requireLogin = require('../middleWares/requireLogin');
+
 module.exports = app => {
-  app.post('/api/stripe', async (req, res) => {
-    const charge = await stripe.charges.create({
-      amount: 500,
-      currency: 'usd',
-      description: '$5 for 5 credits',
-      source: req.body.id,
-    });
-    req.user.credits += 5;
-    const user = await req.user.save();
-    res.send(user);
-  });
+  app.post(
+    '/api/stripe',
+    requireLogin,
+    async (req, res) => {
+      const charge = await stripe.charges.create({
+        amount: 500,
+        currency: 'usd',
+        description: '$5 for 5 credits',
+        source: req.body.id,
+      });
+      req.user.credits += 5;
+      const user = await req.user.save();
+      res.send(user);
+    },
+  );
 };
