e7957bdecedc20052bf65f1f5762e3f1c633e718
Andrew Locke <alocke12992@gmail.com>
2018-05-08 10:50:20 -0600

    Connects client google login

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -3,5 +3,5 @@ export const FETCH_USER = 'FETCH_USER';
 
 export const fetchUser = () => async dispatch => {
   const res = await axios.get('/api/current_user');
-  dispatch({ type: FETCH_USER, payload: res });
+  dispatch({ type: FETCH_USER, payload: res.data });
 };
--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,25 +1,40 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 class Header extends React.Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login with Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a>Logout</a>
+          </li>
+        );
+    }
+  }
   render() {
     return (
       <nav>
         <div className="nav-wrapper blue accent-2">
           <a className="brand-logo">Emaily</a>
-          <ul className="right">
-            <li>
-              <a>Login with Google</a>
-            </li>
-          </ul>
+          <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
     );
   }
 }
-const styles = {
-  logo: {
-    marginLeft: '10x !important',
-  },
+const mapStateToProps = ({ auth }) => {
+  return {
+    auth,
+  };
 };
 
-export default Header;
+export default connect(mapStateToProps)(Header);
