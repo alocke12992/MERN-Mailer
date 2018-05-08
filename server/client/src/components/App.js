ede9f881c594af1376c82cbc8a247d71a53a58aa
Andrew Locke <alocke12992@gmail.com>
2018-05-07 23:44:47 -0600

    Adds fetch user action

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,31 +1,38 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 import Header from './Header';
 import Dashboard from './Dashboard';
 import Landing from './Landing';
 import SurveyNew from './SurveyNew';
 import NoMatch from './NoMatch';
+import * as actions from '../actions';
 
-const App = () => {
-  return (
-    <div className="container">
-      <Header />
-      <Switch>
-        <Route exact path="/" component={Landing} />
-        <Route
-          exact
-          path="/surveys"
-          component={Dashboard}
-        />
-        <Route
-          exact
-          path="/surveys/new"
-          component={SurveyNew}
-        />
-        <Route component={NoMatch} />
-      </Switch>
-    </div>
-  );
-};
+class App extends React.Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+  render() {
+    return (
+      <div className="container">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route
+            exact
+            path="/surveys"
+            component={Dashboard}
+          />
+          <Route
+            exact
+            path="/surveys/new"
+            component={SurveyNew}
+          />
+          <Route component={NoMatch} />
+        </Switch>
+      </div>
+    );
+  }
+}
 
-export default App;
+export default connect(null, actions)(App);
