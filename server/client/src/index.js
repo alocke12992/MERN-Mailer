342f266125edcb5be46d0c05b093210cba5d5630
Andrew Locke <alocke12992@gmail.com>
2018-05-07 22:27:23 -0600

    Adds auth reducer

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
-const store = createStore(() => [], {}, applyMiddleware());
+import App from './components/App';
+import reducers from './reducers';
+
+const store = createStore(reducers, {}, applyMiddleware());
 
 ReactDOM.render(
   <Provider store={store}>
