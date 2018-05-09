41b7957ccd591c7b59e88f79b448dd6c62bb6aab
Andrew Locke <alocke12992@gmail.com>
2018-05-09 12:50:21 -0600

    Fixes survey action bug

--- server/client/src/components/surveys/SurveyField.js
+++ server/client/src/components/surveys/SurveyField.js
@@ -1,42 +1,15 @@
-// SurveyFormReview shows users their form inputs for review
-import _ from 'lodash';
+// SurveyField contains logic to render a single
+// label and text input
 import React from 'react';
-import { connect } from 'react-redux';
-import formFields from './formFields';
-import { withRouter } from 'react-router-dom';
-import * as actions from '../../actions';
 
-const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) => {
-  const reviewFields = _.map(formFields, ({ name, label }) => {
-    return (
-      <div key={name}>
-        <label>{label}</label>
-        <div>{formValues[name]}</div>
-      </div>
-    );
-  });
-
+export default ({ input, label, meta: { error, touched } }) => {
   return (
     <div>
-      <h5>Please confirm your entries</h5>
-      {reviewFields}
-      <button
-        className="yellow darken-3 white-text btn-flat"
-        onClick={onCancel}>
-        Back
-      </button>
-      <button
-        onClick={() => submitSurvey(formValues, history)}
-        className="green btn-flat right white-text">
-        Send Survey
-        <i className="material-icons right">email</i>
-      </button>
+      <label>{label}</label>
+      <input {...input} style={{ marginBottom: '5px' }} />
+      <div className="red-text" style={{ marginBottom: '20px' }}>
+        {touched && error}
+      </div>
     </div>
   );
 };
-
-function mapStateToProps(state) {
-  return { formValues: state.form.surveyForm.values };
-}
-
-export default connect(mapStateToProps, actions)(withRouter(SurveyFormReview));
