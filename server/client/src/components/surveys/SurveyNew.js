878f092070575fe8e87a7f8010a461e841292193
Andrew Locke <alocke12992@gmail.com>
2018-05-09 15:26:50 -0600

    Adds localtunnel for webhooks

--- server/client/src/components/surveys/SurveyNew.js
+++ server/client/src/components/surveys/SurveyNew.js
@@ -9,18 +9,9 @@ class SurveyNew extends Component {
 
   renderContent() {
     if (this.state.showFormReview) {
-      return (
-        <SurveyFormReview
-          onCancel={() => this.setState({ showFormReview: false })}
-        />
-      );
+      return <SurveyFormReview onCancel={() => this.setState({ showFormReview: false })} />;
     }
-
-    return (
-      <SurveyForm
-        onSurveySubmit={() => this.setState({ showFormReview: true })}
-      />
-    );
+    return <SurveyForm onSurveySubmit={() => this.setState({ showFormReview: true })} />;
   }
 
   render() {
