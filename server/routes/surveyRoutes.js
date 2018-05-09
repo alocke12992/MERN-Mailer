8ddbbeac2739cb2d26256ac3d98003d9db50129a
Andrew Locke <alocke12992@gmail.com>
2018-05-08 23:48:28 -0600

    Removes credit on send

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -6,24 +6,25 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-  app.post(
-    '/api/surveys',
-    requireLogin,
-    requireCredits,
-    (req, res) => {
-      const { title, subject, body, recipients } = req.body;
-      const survey = new Survey({
-        title,
-        subject,
-        body,
-        recipients: recipients
-          .split(',')
-          .map(email => ({ email: email.trim() })),
-        _user: req.user.id,
-        dateSent: Date.now(),
-      });
-      const mailer = new Mailer(survey, surveyTemplate(survey));
-      mailer.send();
-    },
-  );
+  app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
+    const { title, subject, body, recipients } = req.body;
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      recipients: recipients.split(',').map(email => ({ email: email.trim() })),
+      _user: req.user.id,
+      dateSent: Date.now(),
+    });
+    const mailer = new Mailer(survey, surveyTemplate(survey));
+    try {
+      await mailer.send();
+      await survey.save();
+      req.user.credits -= 1;
+      const user = await req.user.save();
+      res.send(user);
+    } catch (err) {
+      res.status(422).send(err);
+    }
+  });
 };
