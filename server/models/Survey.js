0668f587630a627337f1c8fa1e739d54d0002c88
Andrew Locke <alocke12992@gmail.com>
2018-05-08 19:13:24 -0600

    Adds Recipient

--- server/models/Survey.js
+++ server/models/Survey.js
@@ -1,12 +1,20 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const RecipientSchema = require('./Recipient');
 
 const surveySchema = new Schema({
   title: String,
   body: String,
   subject: String,
   //Specifies that we want an array of strings
-  recipients: [String],
+  recipients: [RecipientSchema],
+  // Record the total number of times recipients have selected the option
+  yes: { type: Number, default: 0 },
+  no: { type: Number, default: 0 },
+  // use _ to show this is a reference field
+  _user: { type: Schema.Types.ObjectId, ref: 'User' },
+  dateSent: Date,
+  lastResponded: Date,
 });
 
 mongoose.model('surveys', surveySchema);
