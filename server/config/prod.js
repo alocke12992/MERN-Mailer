e6a88b61e82e1ff7ec73fc54d629a6b391c0c790
Andrew Locke <alocke12992@gmail.com>
2018-05-10 12:19:35 -0600

    fixes .env Prod file bug

--- server/config/prod.js
+++ server/config/prod.js
@@ -6,6 +6,6 @@ module.exports = {
   cookieKey: process.env.COOKIE_KEY,
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
   stripeSecretKey: process.env.STRIPE_SECRET_KEY,
-  sendGridKey: process.end.SEND_GRID_KEY,
-  redirectDomain: process.end.REDIRECT_DOMAIN,
+  sendGridKey: process.env.SEND_GRID_KEY,
+  redirectDomain: process.env.REDIRECT_DOMAIN,
 };
