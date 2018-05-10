53eadf117da3a26b181a71d0e5a5e2508394b75a
Andrew Locke <alocke12992@gmail.com>
2018-05-10 12:10:36 -0600

    Shows surveys on dash

--- server/client/src/components/Dashboard.js
+++ server/client/src/components/Dashboard.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
+import SurveyList from './surveys/SurveyList';
 
-const Dashboard = () => (
-  <div>
-    Dashboard
-    <div className="fixed-action-btn">
-      <Link to="/surveys/new" className="btn-floating btn-large red">
-        <i className="material-icons">add</i>
-      </Link>
+const Dashboard = () => {
+  return (
+    <div>
+      <SurveyList />
+      <div className="fixed-action-btn">
+        <Link to="/surveys/new" className="btn-floating btn-large red">
+          <i className="material-icons">add</i>
+        </Link>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Dashboard;
