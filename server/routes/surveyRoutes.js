ff14945f2a08ed1b5e31ee1f3a81396a95609d04
Andrew Locke <alocke12992@gmail.com>
2018-05-10 09:09:10 -0600

    Adds webhook

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -24,6 +24,7 @@ module.exports = app => {
   });
 
   app.post('/api/surveys/webhooks', (req, res) => {
+    console.log(req.body);
     const p = new Path('/api/surveys/:surveyId/:choice');
 
     _.chain(req.body)
